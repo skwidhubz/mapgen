300d7b32d997d4f1ca4a7aa13034180b006b0075
skwidhubz <tommaxwellparker@gmail.com>
2023-11-25 21:25:36 +1100

    random math

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const canvas = document.getElementById("map-canvas");
+const generate_button = document.getElementById("generate-button");
 
 const ctx = canvas.getContext("2d");
 ctx.strokeRect(0, 0, canvas.width, canvas.height);
-ctx.fillStyle = "royalblue";
-ctx.fillRect(400, 100, 200, 200);
-ctx.fillRect(100, 500, 200, 200);
+ctx.fillStyle = "red";
+
+
+function xyRandomGen () {
+    let max = Math.random() * 700;
+    let min = Math.random() * 100;
+    let random = Math.floor(Math.random() * (max - min) + min)
+    return {random}
+}
+
+
+function generateShape () {
+    const randomVal = xyRandomGen()
+    ctx.fillRect(randomVal.random, randomVal.random, randomVal.random, randomVal.random);
+    console.log(randomVal);
+
+};
+
+generate_button.addEventListener("click", generateShape);
+
+
+
+
