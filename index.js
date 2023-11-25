488f61ba06bc967f935129daa196f425133655fe
skwidhubz <tommaxwellparker@gmail.com>
2023-11-25 21:35:23 +1100

    stroke not rect

--- index.js
+++ index.js
@@ -9,14 +9,28 @@ ctx.fillStyle = "red";
 function xyRandomGen () {
     let max = Math.random() * 700;
     let min = Math.random() * 100;
-    let random = Math.floor(Math.random() * (max - min) + min)
-    return {random}
-}
+    let random1 = Math.floor(Math.random() * (max - min) + min);
+    let random2 = Math.ceil(Math.random() * (max - min) + min);
+    return {random1, random2}
+};
 
+function getRandomArray() {
+    const randomLength = Math.floor(Math.random() * 10) + 1;
+    const randomArray = Array.from({ length: randomLength }, () => Math.random());
+    return randomArray;
+};
 
 function generateShape () {
     const randomVal = xyRandomGen()
-    ctx.fillRect(randomVal.random, randomVal.random, randomVal.random, randomVal.random);
+    const countArray = getRandomArray();
+
+
+    for (let i = 0; i < countArray.length; i++) {
+        ctx.strokeRect(randomVal.random1, randomVal.random2, randomVal.random2, randomVal.random2);  
+    };
+    
+    console.log(countArray);
+
     console.log(randomVal);
 
 };
